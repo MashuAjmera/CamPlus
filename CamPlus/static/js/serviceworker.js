f33634a887b00877c41da81a01b90714c49c03a1
Mashu Ajmera <mashuajmera@gmail.com>
2018-08-20 13:22:49 +0530

    Front End Shadow in Cards

--- CamPlus/static/js/serviceworker.js
+++ CamPlus/static/js/serviceworker.js
@@ -20,7 +20,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
-      if ((requestUrl.pathname === '')) {
+      if ((requestUrl.pathname === '/')) {
         event.respondWith(caches.match(filesToCache));
         return;
       }
